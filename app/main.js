999a28aee2b1f63c626d16d0a72ac99a9176abbe
maksr51314 <maksr51314@gmail.com>
2015-05-16 21:27:43 +0300

    fixed client

---
+++ app/main.js
@@ -0,0 +1,26 @@
+//get socket
+var socket = io();
+
+//on submit in form <- THIS IS BROWSER Functionality
+//      $('form').submit(function(){
+
+function sendMe() {
+    //GET YOUR F*CKING message
+    var messageText = $('#m').val();
+
+    //SEND MESSAGE TO THE SERVER
+    socket.emit('chat message', messageText);
+
+    //reset value
+    $('#m').val('');
+}
+
+//when message on the server
+socket.on('chat message', function(msg){
+
+    //show it on the page
+    $('#messages').append($('<li>').text(msg));
+
+    console.log(msg);
+
+});
--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+
+app.use(express.static(__dirname + '/app'));
+
+//console.log(__dirname + '/app' + 'asdsSDADASD');
+//app.configure(function(){
+  //express.use('/media', express.static(__dirname + '/media'));
+  //app.use(express.static(__dirname + '/app'));
+//});
+
+//app.get('/', function(req, res){
+//  res.sendFile(__dirname + '/index.html');
+//});
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
