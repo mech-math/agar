d5f492f19b3a4e40f28e85ed2056c54cda3158f2
maksr51314 <maksr51314@gmail.com>
2015-05-17 22:26:49 +0300

    Fixed position for user

--- app/main.js
+++ app/main.js
@@ -2,10 +2,19 @@
 
 
 var socket = io("http://19fae1e7.ngrok.com");
+var users = [];
+var userId = undefined;
 //var socket = io();
 
 //on submit in form <- THIS IS BROWSER Functionality
 //      $('form').submit(function(){
+
+function register() {
+    var name = $('#name').val();
+
+    socket.emit('general:auth', name)
+}
+
 
 function sendMe() {
     //GET YOUR F*CKING message
@@ -21,9 +30,25 @@ function sendMe() {
 //when message on the server
 socket.on('chat message', function(msg){
 
-    //show it on the page
-    $('#messages').append($('<li>').text(msg));
+    if (users) {
+        var userName = users.filter(function(user) {
+            return user.id === userId;
+        })[0].name;
 
-    console.log(msg);
+        //show it on the page
+        $('#messages').append($('<li>').text(userName + ' : ' + msg));
 
+    }
 });
+
+//when message on the server
+socket.on('general:authComplete', function(usersFromServer){
+    users = usersFromServer;
+
+    if (!userId) {
+        userId = usersFromServer[usersFromServer.length - 1].id
+    }
+
+    console.log(userId);
+});
+
--- index.js
+++ index.js
@@ -3,6 +3,15 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var User = function(id, name) {
+  this.x = 0;
+  this.y = 0;
+  this.id = id;
+  this.name = name;
+};
+
+var users = [];
+
 
 app.use(express.static(__dirname + '/app'));
 
@@ -20,6 +29,15 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
+
+  socket.on('general:auth', function(name){
+    var user = new User(users.length, name);
+
+    users.push(user);
+    io.emit('general:authComplete', users);
+
+  });
+
 });
 
 http.listen(8080, function(){
